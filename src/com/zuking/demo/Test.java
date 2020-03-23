eef417782fc465553bda37472868bcdee910ee2e
zuking <zukingyu@163.com>
2020-03-24 00:03:04 +0800

    test

---
+++ src/com/zuking/demo/Test.java
@@ -0,0 +1,10 @@
+package com.zuking.demo;
+
+/**
+ * Created by zuking on 2020/3/24.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
